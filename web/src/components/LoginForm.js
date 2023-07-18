508a24cfae8262293ff4e2d026a3eb4bfa055a53
ChiuYukSum <11790369+Aiayw@users.noreply.github.com>
2023-07-18 14:35:22 +0800

    fork

--- web/src/components/LoginForm.js
+++ web/src/components/LoginForm.js
@@ -135,7 +135,7 @@ const LoginForm = () => {
         </Message>
         {status.github_oauth || status.wechat_login ? (
           <>
-            <Divider horizontal>开发者可选Github登录注册</Divider>
+            <Divider horizontal>开发者Github登录/注册</Divider>
             {status.github_oauth ? (
               <Button
                 circular
